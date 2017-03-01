d9da12b940e75575cd024a19f8c2f838c95c699f
Maxim <lmakcl1405@gmail.com>
2017-03-01 16:24:14 +0500

    changed name

---
+++ Portal2D/Definitions.h
@@ -0,0 +1,66 @@
+﻿#pragma once
+
+//------TEXTURES------//
+#define HERO '\2'// looks like ☻
+#define HERO_SYMBOL 'H'
+#define BLOCK_SHARP '#'
+#define BLOCK static_cast<char>(219) // looks like █
+#define EMPTY_SPACE ' '
+//#define TURRET ''
+//#define TIMER_BUTTON ''
+//#define STAY_ON_BUTTON ''
+#define RED_PORTAL 'O'
+#define BLUE_PORTAL '0'
+#define AIM static_cast<char>(250);
+#define AIM_DOT '.'
+#define BLACK_WALL static_cast<char>(176)
+#define BLACK_WALL_S 'X'
+#define EXIT static_cast<char>(178)
+#define EXIT_S 'E'
+
+//------MAP------//
+#define NEW_LINE '\n'
+#define MAP_HEIGHT 20
+#define MAP_WIDTH 35
+
+//------MOVE_KEYS------//
+#define W_LOWER_CASE 119
+#define A_LOWER_CASE 97
+#define S_LOWER_CASE 115
+#define D_LOWER_CASE 100
+
+#define W_UPPER_CASE 87
+#define A_UPPER_CASE 65
+#define S_UPPER_CASE 83
+#define D_UPPER_CASE 68
+
+#define W_UPPER_CASE_RU 150
+#define A_UPPER_CASE_RU 148
+#define S_UPPER_CASE_RU 155
+#define D_UPPER_CASE_RU 130
+
+#define W_LOWER_CASE_RU 230
+#define A_LOWER_CASE_RU 228
+#define S_LOWER_CASE_RU 235
+#define D_LOWER_CASE_RU 162
+
+#define SPACE_JUMP 32
+
+//------AIM_KEYS------//
+#define UP_ARROW 72
+#define LEFT_ARROW 75
+#define DOWN_ARROW 80
+#define RIGHT_ARROW 77
+
+//------PORTAL_GUN_KEYS------//
+#define Q_UPPER_CASE 81
+#define E_UPPER_CASE 69
+#define Q_LOWER_CASE 113
+#define E_LOWER_CASE 101
+#define Q_UPPER_CASE_RU 137
+#define E_UPPER_CASE_RU 147
+#define Q_LOWER_CASE_RU 169
+#define E_LOWER_CASE_RU 227
+
+//------ACTION_KEYS------//
+#define ENTER 13
