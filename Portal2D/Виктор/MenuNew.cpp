c760d94ad2422eca5ecd20086f61189e6f788a44
Andrey <andreykostyuchenko74@gmail.com>
2017-03-03 20:37:40 +0500

    TheBestMenu

--- Portal2D/Виктор/MenuNew.cpp
+++ Portal2D/Виктор/MenuNew.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
@@ -11,13 +10,16 @@ void PlayerChouse2(int Cur);
 void MoveToMenu();
 void MoveToMenu2();
 void MoveToMenu3();
-enum{
+
+enum COLOR {
 	GREEN_COLOR = 10, GREEN_COLOR_DARK = 2
 };
+
 using namespace std;
 
 HANDLE H = GetStdHandle(STD_OUTPUT_HANDLE);
-void Stand(int x, int y, int k, char*str){
+
+void Stand(int x, int y, int k, char*str) {
 	CONSOLE_CURSOR_INFO ci; ci.bVisible = 0; ci.dwSize = 100; SetConsoleCursorInfo(H, &ci); // убрать мигающий курсор
 	setlocale(LC_ALL, "RUS");
 	COORD c;
@@ -42,11 +44,11 @@ void hat()
 
 	};
 	printf("\t");
-	for (int r = 0; r <rowsCount; r++)
+	for (int r = 0; r < rowsCount; r++)
 	{
 		for (int c = 0; c < columnsCount; c++)
 		{
-			
+
 			cout << levelData[r][c];
 		}
 		printf("\n\t");
@@ -62,6 +64,7 @@ void punkt()
 	Stand(30, 11, GREEN_COLOR_DARK, "Records");
 	Stand(30, 13, GREEN_COLOR_DARK, "Exit");
 }
+
 void punkt2()
 {
 	hat();
@@ -73,6 +76,7 @@ void punkt2()
 	Stand(30, 19, GREEN_COLOR_DARK, "Level 5");
 	Stand(30, 21, GREEN_COLOR_DARK, "Back");
 }
+
 void punkt3()
 {
 	hat();
@@ -81,7 +85,6 @@ void punkt3()
 	Stand(30, 13, GREEN_COLOR_DARK, "Back");
 
 }
-
 
 void rewriter80(int a)
 {
@@ -98,6 +101,7 @@ void rewriter80(int a)
 	}
 
 }
+
 void rewriter80part2(int a)
 {
 	switch (a)
@@ -105,19 +109,19 @@ void rewriter80part2(int a)
 	case 2:
 		Stand(30, 9, GREEN_COLOR_DARK, "Random level");
 		Stand(30, 11, GREEN_COLOR, "Level 1");
-		
+
 		break;
 
 	case 3:
 		Stand(30, 11, GREEN_COLOR_DARK, "Level 1");
 		Stand(30, 13, GREEN_COLOR, "Level 2");
-		
+
 		break;
 
 	case 4:
 		Stand(30, 13, GREEN_COLOR_DARK, "Level 2");
 		Stand(30, 15, GREEN_COLOR, "Level 3");
-		
+
 		break;
 
 	case 5:
@@ -137,6 +141,7 @@ void rewriter80part2(int a)
 	}
 
 }
+
 void rewriter80part3(int a)
 {
 	switch (a)
@@ -150,15 +155,12 @@ void rewriter80part3(int a)
 		Stand(30, 13, GREEN_COLOR, "Back");
 		break;
 	}
-
 }
-
 
 void rewriter72(int a)
 {
 	switch (a)
 	{
-		
 	case 1:
 		Stand(30, 9, GREEN_COLOR, "Start game");
 		Stand(30, 11, GREEN_COLOR_DARK, "Records");
@@ -171,13 +173,12 @@ void rewriter72(int a)
 		Stand(30, 13, GREEN_COLOR, "Exit");
 		break;
 	}
-
 }
+
 void rewriter72part2(int a)
 {
 	switch (a)
 	{
-
 	case 1:
 		Stand(30, 9, GREEN_COLOR, "Random level");
 		Stand(30, 11, GREEN_COLOR_DARK, "Level 1");
@@ -206,15 +207,13 @@ void rewriter72part2(int a)
 		Stand(30, 19, 10, "Level 5");
 		Stand(30, 21, GREEN_COLOR_DARK, "Back");
 		break;
-
-
 	}
 }
+
 void rewriter72part3(int a)
 {
 	switch (a)
 	{
-
 	case 1:
 		Stand(30, 9, GREEN_COLOR, "Show all records");
 		Stand(30, 11, GREEN_COLOR_DARK, "Show 10 records");
@@ -227,18 +226,15 @@ void rewriter72part3(int a)
 		Stand(30, 13, GREEN_COLOR, "Back");
 		break;
 	}
-
 }
 
-
-
 // присоединять свои функции только в функциях PlayerChose2 и PlayerChose3, вместо соответствующих текстовых сообщений
+
 void PlayerChouse(int Cur)
 {
 	system("cls");
 	switch (Cur)
 	{
-	
 	case 1: punkt2();
 		MoveToMenu2();
 		break;
@@ -248,13 +244,13 @@ void PlayerChouse(int Cur)
 	case 3: exit(0);
 		break;
 	}
-}                                                                               
+}
+
 void PlayerChouse3(int Cur)
 {
 	system("cls");
 	switch (Cur)
 	{
-
 	case 1: cout << "All Records";
 		break;
 	case 2:cout << "10 Records";
@@ -264,13 +260,13 @@ void PlayerChouse3(int Cur)
 		MoveToMenu();;
 		break;
 	}
-}  
+}
+
 void PlayerChouse2(int Cur)
 {
 	system("cls");
 	switch (Cur)
 	{
-
 	case 1: cout << "Random level";
 		break;
 	case 2:cout << "Level 1";
@@ -285,68 +281,63 @@ void PlayerChouse2(int Cur)
 		break;
 	case 7:
 		punkt();
-
 		MoveToMenu();;
 		break;
 	}
 }
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 void MoveToMenu()
 {
 	int position = 1;
-	
-	while (1){
-int code = _getch();
-		switch (code)
-		{
-		case 80:
-			if (position<3)
-			position++;
-			rewriter80(position);
-			
-			break;
-		case 72:
-			if (position>1)
 
-			position--;
-			rewriter72(position);
-			
-			break;
-		case 13://вниз
-
-			PlayerChouse(position);
-			
-			break;
-		}
-	}
-}
-void MoveToMenu2()
-{
-	int position = 1;
-
-	while (1){
+	while (1)
+	{
 		int code = _getch();
 		switch (code)
 		{
 		case 80:
-			if (position<7)
+			if (position < 3)
+				position++;
+			rewriter80(position);
+			break;
+
+		case 72:
+			if (position > 1)
+				position--;
+			rewriter72(position);
+			break;
+
+		case 13://вниз
+			PlayerChouse(position);
+			break;
+		}
+	}
+}
+
+void MoveToMenu2()
+{
+	int position = 1;
+
+	while (1)
+	{
+		int code = _getch();
+		switch (code)
+		{
+		case 80:
+			if (position < 7)
 				position++;
 			rewriter80part2(position);
-
 			break;
-		case 72:
-			if (position>1)
 
+		case 72:
+			if (position > 1)
 				position--;
 			rewriter72part2(position);
-
 			break;
+
 		case 13:
 			PlayerChouse2(position);
-
-
 			break;
 		}
 	}
@@ -355,27 +346,25 @@ void MoveToMenu3()
 {
 	int position = 1;
 
-	while (1){
+	while (1)
+	{
 		int code = _getch();
 		switch (code)
 		{
 		case 80:
-			if (position<3)
+			if (position < 3)
 				position++;
 			rewriter80part3(position);
-
 			break;
-		case 72:
-			if (position>1)
 
+		case 72:
+			if (position > 1)
 				position--;
 			rewriter72part3(position);
-
 			break;
+
 		case 13:
-
 			PlayerChouse3(position);
-
 			break;
 		}
 	}
